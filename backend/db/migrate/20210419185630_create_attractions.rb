675ce813bf3e91f704e5cd4583da74cdad9ac64b
github username <github email address>
2021-04-19 14:15:25 -0700

    need real API data

--- backend/db/migrate/20210419185630_create_attractions.rb
+++ backend/db/migrate/20210419185630_create_attractions.rb
@@ -1,4 +1,10 @@
 class CreateAttractions < ActiveRecord::Migration[5.2]
   def change
+    create_table :attractions do |t|
+      t.string :name
+      t.string :igUrl
+      t.string :ytUrl
+      t.string :imageUrl
+    end
   end
 end
