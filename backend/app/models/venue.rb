635d444b9b4854195747ad6b9a9fb2a0b6188142
github username <github email address>
2021-04-20 17:16:50 -0700

    setup frontend

--- backend/app/models/venue.rb
+++ backend/app/models/venue.rb
@@ -1,4 +1,14 @@
 class Venue < ActiveRecord::Base
     has_many :events
     has_many :attractions, through: :events
+
+    def self.sort_by_max
+        self.all.sort_by{|venue| venue.events.length}.last
+    end
+
+    def self.sort_by_min
+        self.all.sort_by{|venue| venue.events.length}.first
+    end
 end
+
+# venue with most events
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import EventContainer from './components/EventContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+URL = "http://localhost:9393"
+class App extends React.Component {
+
+  state = {
+    events: [],
+    venues: []
+  }
+
+  componentDidMount() {
+    fetch(`${URL}/events`)
+    .then(res => res.json())
+    .then(eventData => {
+      this.setState({
+        events: eventData
+      })
+    })
+  }
+
+  sort(e) {
+    fetch(`${URL}/${e.target.value}`)
+    .then(res => res.json())
+    .then(dateData => {
+      console.log(dateData)
+      this.setState({
+        events: dateData
+      })
+    })
+  }
+
+  render() {
+    
+    return (
+    
+    <div>
+      <header >Website name</header>
+      <div>
+        <form className="form">
+          <select onChange={(e) => this.sort(e)}>
+            <option value="events">default</option>
+            <option value="dates">dates</option>
+            <option>Ex2</option>
+            <option>Ex3</option>
+            <option>Ex4</option>
+          </select>
+        </form>
+      </div>
+      <div>
+        <EventContainer eventData={this.state.events} />
+      </div>
     </div>
-  );
+  );  
+  }
 }
 
 export default App;
