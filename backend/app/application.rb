f54509f404403a82dc55de95543c11f2c5305d03
Isabella <wileyisabella@gmail.com>
2021-05-12 10:33:29 -0400

    added /seasons/ and /recipes/

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -9,6 +9,23 @@ class Application
       
       return [200, { 'Content-Type' => 'application/json' }, [ seasons.to_json ]]
 
+    elsif req.path.match('/seasons/') && req.get?
+      id = req.path.split("/").last
+
+      produces = Produce.where(season_id: id)
+
+      return [200, { 'Content-Type' => 'application/json' }, [ produces.to_json ]]
+
+    elsif req.path.match('/recipes/') && req.get?
+      id = req.path.split("/").last
+
+      produces = Produce.where(season_id: id)
+
+      recipes = []
+      produces.map{|produce| recipes.push(produce.recipe)}
+
+      return [200, { 'Content-Type' => 'application/json' }, [ recipes.uniq.to_json ]]
+
     else
       resp.write "Path Not Found"
 
