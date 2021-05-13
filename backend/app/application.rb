da1597e188d99c09eebdabd27c9ea59afc9c5de1
esellers25 <erin.e.sellers@gmail.com>
2021-05-13 15:52:22 -0400

    form working shopping list working

--- backend/app/application.rb
+++ backend/app/application.rb
@@ -1,3 +1,5 @@
+require "Pry"
+
 class Application
 
   def call(env)
@@ -28,12 +30,33 @@ class Application
 
       return [200, { 'Content-Type' => 'application/json' }, [ recipes.uniq.to_json ]]
 
+
     elsif req.path.match("/recipes") && req.post?
       hash = JSON.parse(req.body.read)
         
-      new_recipe = Recipe.create(hash)
-      
+      new_recipe = Recipe.create(name: hash["name"], image: hash["image"], link: hash["link"], difficulty: hash["difficulty"], description: hash["description"])
+      produce_arr = hash["produceArr"].map{|produce_obj| produce_obj["value"]}
+
+      produce_ids = produce_arr.map{|produce| Produce.find_by(name: produce).id}
+
+      new_produce_recipes = produce_ids.map{|produce_id| ProduceRecipe.create(recipe_id: new_recipe.id, produce_id: produce_id)}
+     
       return [201, { 'Content-Type' => 'application/json' }, [ new_recipe.consistent_data ]]
+
+
+    elsif req.path == ('/produces') && req.get?
+      produce = Produce.where(on_list: true)
+
+      return [200, { 'Content-Type' => 'application/json' }, [ produce.to_json ]]
+    
+    elsif req.path.match('/produces/') && req.patch?
+      hash = JSON.parse(req.body.read)
+      id = req.path.split("/produces/").last
+
+      produce = Produce.find(id)
+      produce.update(hash)
+
+      return [201, { 'Content-Type' => 'application/json' }, [ produce.to_json ]]
 
     else
       resp.write "Path Not Found"
